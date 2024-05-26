3bf70d2470266e32dacdbd13bed36de0cb1cfb4b
chloe0524 <chloco31@gmail.com>
2024-05-26 20:21:48 +0200

    task 1

---
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -0,0 +1,14 @@
+/* global $ */
+/* A = amenities */
+$(document).ready(() => {
+    const reviewedA = {};
+  
+    $('input[type="checkbox"]').change(function () {
+      const AId = $(this).data('id');
+      const AName = $(this).data('name');
+  
+      $(this).is(':checked') ? reviewedA[AId] = aName : delete reviewedA[AId];
+  
+      $('.amenities h4').text(Object.values(reviewedA).join(', '));
+    });
+  });
