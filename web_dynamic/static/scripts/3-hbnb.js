0dfb64d6fcef5f02fe2cf6118f09a30d36e14117
chloe0524 <chloco31@gmail.com>
2024-05-27 16:53:00 +0200

    task 3 json article with api port 5001

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,44 @@
+/* global $ */
+/* A = amenities */
+$(document).ready(() => {
+  const reviewedA = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const AId = $(this).data('id');
+    const AName = $(this).data('name');
+
+    if ($(this).is(':checked')) {
+      reviewedA[AId] = AName;
+    } else {
+      delete reviewedA[AId];
+    }
+    $('.amenities h4').text(Object.values(reviewedA).join(', '));
+  });
+});
+$(document).ready(() => {
+  $.get('http://0.0.0.0:5001/api/v1/status/', (data, textStatus) => {
+    if (textStatus === 'success') {
+      $('#api_status').toggleClass('available', data.status === 'OK');
+    }
+  });
+});
+$(document).ready(() => {
+  $.ajax({
+    url: 'http://0.0.0.0:5001/api/v1/places_search/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify({}),
+    success: function (data) {
+      for (const place of data) {
+        const article = $('<article></article>');
+        const title = $('<div class="title"></div>');
+        title.append('<h2>' + place.name + '</h2>');
+        title.append('<div class="price_by_night">' + place.price_by_night + '</div>');
+        article.append(title);
+        article.append('<div class="information"></div>');
+        article.append('<div class="description">' + place.description + '</div>');
+        $('.places').append(article);
+      }
+    }
+  });
+});
