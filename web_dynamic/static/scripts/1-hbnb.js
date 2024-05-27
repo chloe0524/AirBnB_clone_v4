5d74525dc21a1497b706804903048d91d16cc593
chloe0524 <chloco31@gmail.com>
2024-05-27 10:37:03 +0200

    semistzndard fix

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,13 @@
 /* global $ */
 /* A = amenities */
 $(document).ready(() => {
-    const reviewedA = {};
-  
-    $('input[type="checkbox"]').change(function () {
-      const AId = $(this).data('id');
-      const AName = $(this).data('name');
-  
-      $(this).is(':checked') ? reviewedA[AId] = aName : delete reviewedA[AId];
-  
-      $('.amenities h4').text(Object.values(reviewedA).join(', '));
-    });
+  const reviewedA = {};
+
+  $('input[type="checkbox"]').change(function () {
+    const AId = $(this).data('id');
+    const AName = $(this).data('name');
+
+    $(this).is(':checked') ? reviewedA[AId] = AName : delete reviewedA[AId];
+    $('.amenities h4').text(Object.values(reviewedA).join(', '));
   });
+});
